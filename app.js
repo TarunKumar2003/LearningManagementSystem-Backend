8404653080aaa3b7755a6cc499261cb04b6da817
Tarun Kumar <tarunnimbal123@gmail.com>
2024-03-31 16:27:21 +0530

    Download all dependencies and add a simple route ping to it

---
+++ app.js
@@ -0,0 +1,33 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+
+// configure middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// third party middleware
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    credentials:true
+}))
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+app.get('/ping',(req,res)=>{
+    res.send('pong');
+})
+
+//app.use('./api/v1/user',userRouter);
+
+app.all('*',(req,res)=>{
+    res.status(404).send("OOPS !! 404 Page not found");
+})
+
+export default app;
